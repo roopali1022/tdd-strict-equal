ce1125d8e98ad0e3fad342808a463ae5a80a023a
roopali1022 <roopali.lucky@gmail.com>
2020-09-29 21:24:49 -0400

    Added package.json and dependencies. Created failing test for sumAll

---
+++ tests/calculation.test.js
@@ -0,0 +1,14 @@
+var describe = require('mocha').describe
+var it = require('mocha').it
+
+describe('calculation', () => {
+  describe('sumAll', () => {
+    it('returns the sum of the numbers in the array provided', () => { 
+      // setup test data
+
+      // call the function with the test data
+
+      // asserts what should be true
+    })
+  })
+})
